e210cbf149df84639acc8fae697776371d0807c3
Shivam Mishra <scm.mymail@gmail.com>
2019-09-22 19:18:04 +0530

    fix: added height

--- src/configurators/AxisChart.vue
+++ src/configurators/AxisChart.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="flex flex-wrap -mx-3 mb-4">
       <Data label="Title" placeholder="Your Awesome Title" v-model="chartConfig.title" width="full"/>
-      <!-- <Data label="Height" placeholder="300" v-model="chartConfig.height" width="third"/> -->
+      <Data label="Height" placeholder="300" v-model="chartConfig.height" width="third"/>
     </div>
     <div class="flex flex-wrap -mx-3 mb-4">
       <Data
