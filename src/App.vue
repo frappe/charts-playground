eaa9f294480c5bfc2bdacdf294b493c3716d1776
Shivam Mishra <scm.mymail@gmail.com>
2020-09-18 16:56:43 +0530

    feat: improvements to layout

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,70 @@
 <template>
-  <div class="flex">
-    <div
-      class="w-1/4 flex flex-col justify-between bg-white overflow-hidden md:h-screen min-h-full shadow-lg p-5"
-    >
-      <div class="chart-config">
-        <Title/>
-        <Options label="Chart Type" :options="chartTypes" width="full" v-model="chartType"/>
-        <component :is="currentComponent" @updateChart="updateChart"/>
-      </div>
-    </div>
-    <div class="w-3/4 p-5">
-      <div class="bg-white rounded shadow-lg">
-        <div id="chart"></div>
-      </div>
-    </div>
-  </div>
+	<div class="flex m-5">
+		<div
+			class="w-1/4 flex flex-col justify-between bg-white overflow-hidden shadow-lg p-5 rounded-lg"
+		>
+			<div class="chart-config">
+				<Title />
+				<Options
+					label="Chart Type"
+					:options="chartTypes"
+					width="full"
+					v-model="chartType"
+				/>
+				<component :is="currentComponent" @updateChart="updateChart" />
+			</div>
+		</div>
+		<div class="w-3/4 ml-5">
+			<div class="bg-white rounded shadow-lg">
+				<div id="chart"></div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import { Chart } from "frappe-charts/dist/frappe-charts.min.esm";
-import AxisChart from "@/configurators/AxisChart"
-import BarChart from "@/configurators/BarChart"
-import LineChart from "@/configurators/LineChart"
-import Options from "@/controls/Options"
-import Title from "@/components/Title"
+/* eslint-disable */
+import { Chart } from "frappe-charts/dist/frappe-charts.esm";
+import AxisChart from "@/configurators/AxisChart";
+import BarChart from "@/configurators/BarChart";
+import LineChart from "@/configurators/LineChart";
+import Options from "@/controls/Options";
+import Title from "@/components/Title";
 
 export default {
-  name: "app",
-  components: {
-    Title: Title,
-    Options: Options,
-    AxisChart: AxisChart,
-    BarChart: BarChart,
-    LineChart: LineChart
-  },
-  data() {
-    return {
-      chartType: 'bar',
-      chartComponents: {
-        'axis-mixed': AxisChart,
-        'bar': BarChart,
-        'line': LineChart
-      },
-      chartTypes: [
-        {label: "Axis", value: "axis-mixed"},
-        {label: "Bar", value: "bar"},
-        {label: "Line", value: "line"},
-      ]
-    }
-  },
-  methods: {
-    updateChart(config) {
-      window.chart = new Chart("#chart", config);
-    }
-  },
-  computed: {
-    currentComponent() {
-      return this.chartComponents[this.chartType]
-    }
-  }
+	name: "app",
+	components: {
+		Title: Title,
+		Options: Options,
+		AxisChart: AxisChart,
+		BarChart: BarChart,
+		LineChart: LineChart,
+	},
+	data() {
+		return {
+			chartType: "bar",
+			chartComponents: {
+				"axis-mixed": AxisChart,
+				bar: BarChart,
+				line: LineChart,
+			},
+			chartTypes: [
+				{ label: "Axis", value: "axis-mixed" },
+				{ label: "Bar", value: "bar" },
+				{ label: "Line", value: "line" },
+			],
+		};
+	},
+	methods: {
+		updateChart(config) {
+			window.chart = new Chart("#chart", config);
+		},
+	},
+	computed: {
+		currentComponent() {
+			return this.chartComponents[this.chartType];
+		},
+	},
 };
 </script>
 
--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,26 +1,28 @@
 <template>
-      <div id="title">
-        <h1 class="text-center text-xl font-bold">Frappe Charts Playground</h1>
-        <div class="text-center mt-2">
-          <a
-            class="uppercase text-xs text-gray-600 hover:text-gray-800 font-bold p-2"
-            href="https://github.com/frappe/charts"
-          >Github</a>
-          <a
-            class="uppercase text-xs text-gray-600 hover:text-gray-800 font-bold p-2"
-            href="https://frappe.io/charts/docs"
-          >Documentation</a>
-		  <a
-            class="uppercase text-xs text-gray-600 hover:text-gray-800 font-bold p-2"
-            href="https://t.me/frappecharts"
-          >Telegram</a>
-        </div>
-        <hr class="bg-gray-300 h-px w-100 my-5" />
-      </div>
+	<div id="title" class="mb-12 mt-3">
+		<h1 class="text-center text-xl font-bold">Frappe Charts Playground</h1>
+		<div class="text-center mt-2">
+			<a
+				class="uppercase text-xs text-gray-600 hover:text-gray-800 font-bold p-2"
+				href="https://github.com/frappe/charts"
+				>Github</a
+			>
+			<a
+				class="uppercase text-xs text-gray-600 hover:text-gray-800 font-bold p-2"
+				href="https://frappe.io/charts/docs"
+				>Documentation</a
+			>
+			<a
+				class="uppercase text-xs text-gray-600 hover:text-gray-800 font-bold p-2"
+				href="https://t.me/frappecharts"
+				>Telegram</a
+			>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-	name: 'Title'
-}
+	name: "Title",
+};
 </script>
